6e870e84d168e8fb03e9b05feee958a88a1f56c4
JoseSic <jose.sic96@gmail.com>
2018-09-26 18:28:07 -0600

    Inciso 2

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hola mundo");
+var mensaje = "Hola mundo";
+const constante1 = "Hola constante"
+console.log(mensaje);
+
+function MiPrimerMetodo(){
+    return "Hola desde la función";
+}
+function SumaNumeros(x,y)
+{
+    return x+y;
+}
+
+console.log(MiPrimerMetodo());
+console.log(constante1);
+console.log(SumaNumeros(4,2));
