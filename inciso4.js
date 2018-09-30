5af01d07e5f49e66cf8275852ab8a5d7fd983275
JoseSic <jose.sic96@gmail.com>
2018-09-30 17:30:19 -0600

    Servidor

---
+++ inciso4.js
@@ -0,0 +1,21 @@
+///////////////////////////////////////////////////////////////////////////////////
+var finalhandler = require('finalhandler')
+var http = require('http')
+var Router = require('router')
+ 
+var router = Router()
+router.get('/', function (req, res) {
+  res.setHeader('Content-Type', 'text/plain; charset=utf-8')
+  res.end('Mi primer servidor http JOSE SIC!')
+})
+
+router.get('/hello/:name', function (req, res) {
+    res.setHeader('Content-Type', 'application/json')
+    res.end(JSON.stringify({ name: req.params.name}))
+  })
+ 
+var server = http.createServer(function(req, res) {
+  router(req, res, finalhandler(req, res))
+})
+ 
+server.listen(3000)
