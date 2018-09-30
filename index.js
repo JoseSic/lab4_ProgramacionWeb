224d95435e0dec239e0be0e50d4bcdfbf33e0c74
JoseSic <jose.sic96@gmail.com>
2018-09-30 17:04:24 -0600

    Función Validadora

--- index.js
+++ index.js
@@ -1,16 +1,45 @@
-var mensaje = "Hola mundo";
-const constante1 = "Hola constante"
-console.log(mensaje);
 
-function MiPrimerMetodo(){
-    return "Hola desde la función";
-}
-function SumaNumeros(x,y)
-{
-    return x+y;
-}
 
-console.log(MiPrimerMetodo());
-console.log(constante1);
-console.log(SumaNumeros(4,2));
-console.log("Jose Puto");
+function validarString (cadenaAnalizar) {
+    var arrayDummy = new Array();
+    for (var i = 0; i< cadenaAnalizar.length; i++) {
+          var caracter = cadenaAnalizar.charAt(i);
+          var caracter2;
+          if( caracter == "[" || caracter == "(" ||caracter == "{") {
+             arrayDummy.push(caracter);
+           }  else {
+              if (caracter == "]"){
+                  caracter2 = arrayDummy.pop();
+                  if(caracter2 != "["){
+                      return "Cadena incorrecta";
+                  }
+              }else{
+                  if (caracter == ")"){
+                      caracter2 = arrayDummy.pop()
+                      if(caracter2 != "(")
+                      {
+                        return "Cadena incorrecta";
+                      }
+                  }else{
+                    if (caracter == "}"){
+                        caracter2 = arrayDummy.pop()
+                        if(caracter2 != "{")
+                        {
+                          return "Cadena incorrecta";
+                        }
+                  }
+              }
+           }
+     } 
+ }
+
+ if(arrayDummy.length > 0)
+ {
+    return "Cadena incorrecta";
+ }else{
+    return "Cadena correcta";
+ }
+}  
+console.log(validarString("}(2+[3-12]*{8/3})"));
+console.log(validarString("(2+[3-12]*{8/3})"));
+console.log(validarString("{}(2+{3-12*{8/3})"));
